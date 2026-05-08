aa11cf3816a869af3f395a1526709c4567e480ab
nitty fritty <nathan.fredette@libertymutual.com>
2026-05-08 13:19:50 +0000

    add tests and workflow

---
+++ test.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+
+//Test 1: Addition
+assert.strictEqual(2 + 2, 4, "2 + 2 should equal 4");
+
+// Test 2: string
+assert.strictEqual(typeof "hello", "string", "hello should be a string");
+console.log("string test passed");
+
+console.log("\nAll tests passed!");
